c07ab3db54e6885f421613c96371366df9d06dfd
suhacanuluer <sculuer@gmail.com>
2022-06-06 01:49:10 +0300

    requests added

---
+++ src/main/java/com/suhacanuluer/todoapp/request/AddTodoRequest.java
@@ -0,0 +1,20 @@
+package com.suhacanuluer.todoapp.request;
+
+public class AddTodoRequest {
+    private String content;
+
+    public AddTodoRequest() {
+    }
+
+    public AddTodoRequest(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
