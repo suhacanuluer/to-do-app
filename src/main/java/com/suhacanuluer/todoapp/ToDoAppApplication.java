1100b7e549e68e7f95e5902ee9878f37cbb002d3
suhacanuluer <sculuer@gmail.com>
2022-06-06 01:48:57 +0300

    edited for running

--- src/main/java/com/suhacanuluer/todoapp/ToDoAppApplication.java
+++ src/main/java/com/suhacanuluer/todoapp/ToDoAppApplication.java
@@ -1,13 +1,28 @@
 package com.suhacanuluer.todoapp;
 
+import com.suhacanuluer.todoapp.entity.Todo;
+import com.suhacanuluer.todoapp.entity.Users;
+import com.suhacanuluer.todoapp.repository.TodoRepository;
+import com.suhacanuluer.todoapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoAppApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TodoRepository todoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoAppApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
