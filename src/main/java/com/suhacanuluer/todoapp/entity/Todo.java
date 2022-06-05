85a457965e51871a8034ffffa54ae92be31a5ddb
suhacanuluer <sculuer@gmail.com>
2022-06-06 01:46:45 +0300

    todo edited

--- src/main/java/com/suhacanuluer/todoapp/entity/Todo.java
+++ src/main/java/com/suhacanuluer/todoapp/entity/Todo.java
@@ -1,11 +1,14 @@
 package com.suhacanuluer.todoapp.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Todo {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String content;
     private Boolean completed = Boolean.FALSE;
