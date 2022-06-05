d5b875a3d8c10b728a588336f80f9ee4d05b1d00
suhacanuluer <sculuer@gmail.com>
2022-06-05 19:27:49 +0300

    user added

---
+++ src/main/java/com/suhacanuluer/todoapp/entity/User.java
@@ -0,0 +1,60 @@
+package com.suhacanuluer.todoapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @OneToMany
+    private List<Todo> todoList;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, List<Todo> todoList) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.todoList = todoList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Todo> getTodoList() {
+        return todoList;
+    }
+
+    public void setTodoList(List<Todo> todoList) {
+        this.todoList = todoList;
+    }
+}
