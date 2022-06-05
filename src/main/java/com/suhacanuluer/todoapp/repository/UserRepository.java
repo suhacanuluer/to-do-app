c5c7d346cb40b23493669b9edbdf8b49fb4d9210
suhacanuluer <sculuer@gmail.com>
2022-06-06 01:47:13 +0300

    repositories added

---
+++ src/main/java/com/suhacanuluer/todoapp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.suhacanuluer.todoapp.repository;
+
+import com.suhacanuluer.todoapp.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+}
